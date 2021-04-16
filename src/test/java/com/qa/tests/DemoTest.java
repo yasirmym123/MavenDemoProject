8b295903746671ffcad4e3e513853af927c72f27
Yasir Munir <yasir.m@DELINEMEDIA.LOCAL>
2021-04-16 21:27:16 +0500

    First Commit

---
+++ src/test/java/com/qa/tests/DemoTest.java
@@ -0,0 +1,28 @@
+package com.qa.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	WebDriver driver;
+	
+	
+	@Test
+	public void LaunchBrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\yasir.m\\eclipse-workspace\\TestProject\\chromedriver.exe");
+		
+		ChromeOptions options= new ChromeOptions();
+		options.setHeadless(true);
+		driver = new ChromeDriver(options);
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("Automation Step By Step");
+		driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']")).click();
+		System.out.println(driver.getTitle());
+		
+	}
+
+}
